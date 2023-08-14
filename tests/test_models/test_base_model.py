22028240052420c51bbc3ae3755d70980ddfc2f8
sirln <siro@sirlawren.com>
2023-08-14 15:42:59 +0300

    AirBnB Clone - The Console. Adding test_base_model module, for BaseModel class unittesting

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+BaseModel Class Unit Test Module
+'''
+import models
+import unittest
+from uuid import uuid4
+from datetime import  datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+
+# ----------------------- Docstring Testing -------------------------
+    def test_module_docstring(self):
+        '''
+        Test if base_model module has a docstring.
+        '''
+        doc = models.base_model.__doc__
+        self.assertIsNotNone(doc, 'Module should have a docstring')
+        self.assertTrue(len(doc.strip()) > 0, 'Module docstring should not be empty')
+
+    def test_class_docstring(self):
+        '''
+        Test if the class BaseModel has a docstring.
+        '''
+        doc = BaseModel.__doc__
+        self.assertIsNotNone(doc, 'Class BaseModel should have a docstring')
+        self.assertTrue(len(doc.strip()) > 0, 'Class BaseModel docstring should not be empty')
+
+    def test_class_methods_docstring(self):
+        '''
+        Test if class methods have docstrings
+        '''
+        class_methods = [
+                            method for method in dir(BaseModel)
+                            if (not method.startswith('__')
+                            or method == '__str__')
+                            and callable(getattr(BaseModel, method))
+                        ]
+        for method in class_methods:
+            doc = getattr(BaseModel, method).__doc__
+            self.assertIsNotNone(doc, f'Method {method} should have a docstring')
+            self.assertTrue(len(doc.strip()) > 0, f'Method {method} docstring should not be empty')
+
+
+# ----------------------- Instantiation Testing ----------------------
+    def test_base_model_init_with_kwargs(self):
+        """Tests initialization of BaseModel with kwargs."""
+        _id = str(uuid4())
+        time = datetime.now()
+        iso_time = time.isoformat()
+        b = BaseModel(id=_id, created_at=iso_time, updated_at=iso_time)
+        self.assertEqual(b.id, _id)
+        self.assertEqual(b.created_at, time)
+        self.assertEqual(b.updated_at, time)
+
+if __name__ == '__main__':
+    unittest.main()
