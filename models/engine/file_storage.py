42058f0331c2e746ae333e75c53ed210ca759368
sirln <siro@sirlawren.com>
2023-08-14 20:14:01 +0300

    AirBnB Clone - The Console. Modifications to the user and file_storage modules

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,12 @@ FileStorage Module
 import json
 from os import path
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
--- models/user.py
+++ models/user.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 
+"""
+A User module that inherits the characteristics
+from the class BaseModel
+"""
 from models.base_model import BaseModel
 
 
-"""A User module that inherits the characteristics
-from the class BaseModel"""
-
-
 class User(BaseModel):
-    """User class that inherits from BaseModel
+    """
+    User class that inherits from BaseModel
 
     Attributes
     ----------
 
     email: str
-        This refers to the email address
-        of the person
+        This refers to the email address of the person
     password: str
-        This refers to the password of
-        the person
+        This refers to the password of the person
     first_name: str
-        This refers to the person's first
-        name
+        This refers to the person's first name
     last_name: str
-        This refers to the person's last
-        name
+        This refers to the person's last name
 
     """
 
